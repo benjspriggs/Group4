2982d4beac79fc50e4afc5efdcba0eadaf31fa9b
benjspriggs <benjspriggs@sprico.com>
2016-11-15 22:45:53 -0800

    add stopping condition for repl

--- src/main/scala/interactive/ReadEvaluatePrintLoop.scala
+++ src/main/scala/interactive/ReadEvaluatePrintLoop.scala
@@ -4,7 +4,10 @@ import java.io._
 import java.util.function.Consumer
 
 import fastparse.core.Parsed
+import fastparse.core.Parsed.{Failure, Success}
 import interactive.parser.Parser
+
+import scala.Serializable
 
 /**
   * Created by bspriggs on 11/15/2016.
@@ -14,6 +17,11 @@ class ReadEvaluatePrintLoop {
 
   def parseInput(s: String) = parser.parse(s)
 
+  def continue(t: Parsed[Product with Serializable, Char, String]): Boolean = t match {
+    case _: Success[Product with Serializable, Char, String] => false
+    case _ => true
+  }
+
   def start(input: InputStream, output: OutputStream) = {
     // Run the repl loop
     val reader = new BufferedReader(new InputStreamReader(input))
@@ -21,6 +29,7 @@ class ReadEvaluatePrintLoop {
     Stream.continually(reader readLine)
       .takeWhile(_ != null)
       .map(parser.parse(_))
+      .takeWhile(continue)
       .foreach((s: Parsed[Product with Serializable,_,_]) => { writer write s.toString; writer flush();})
   }
 }
