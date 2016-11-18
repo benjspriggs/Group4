10e2d9431518c351552e9eb03a759291451ded1f
benjspriggs <benjspriggs@sprico.com>
2016-11-17 19:56:26 -0800

    make json parser properly lazy

--- src/main/scala/interactive/parser/JsonParser.scala
+++ src/main/scala/interactive/parser/JsonParser.scala
@@ -42,37 +42,37 @@ object JsonParser {
   val Digits = NamedFunction('0' to '9' contains (_: Char), "Digits")
   val StringChars = NamedFunction(!"\"\\".contains(_: Char), "StringChars")
 
-  val space         = P( CharsWhile(Whitespace).? )
-  val digits        = P( CharsWhile(Digits))
-  val exponent      = P( CharIn("eE") ~ CharIn("+-").? ~ digits )
-  val fractional    = P( "." ~ digits )
-  val integral      = P( "0" | CharIn('1' to '9') ~ digits.? )
+  lazy val space         = P( CharsWhile(Whitespace).? )
+  lazy val digits        = P( CharsWhile(Digits))
+  lazy val exponent      = P( CharIn("eE") ~ CharIn("+-").? ~ digits )
+  lazy val fractional    = P( "." ~ digits )
+  lazy val integral      = P( "0" | CharIn('1' to '9') ~ digits.? )
 
-  val number = P( CharIn("+-").? ~ integral ~ fractional.? ~ exponent.? ).!.map(
+  lazy val number = P( CharIn("+-").? ~ integral ~ fractional.? ~ exponent.? ).!.map(
     x => Js.Num(x.toDouble)
   )
 
-  val `null`        = P( "null" ).map(_ => Js.Null)
-  val `false`       = P( "false" ).map(_ => Js.False)
-  val `true`        = P( "true" ).map(_ => Js.True)
+  lazy val `null`        = P( "null" ).map(_ => Js.Null)
+  lazy val `false`       = P( "false" ).map(_ => Js.False)
+  lazy val `true`        = P( "true" ).map(_ => Js.True)
 
-  val hexDigit      = P( CharIn('0'to'9', 'a'to'f', 'A'to'F') )
-  val unicodeEscape = P( "u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit )
-  val escape        = P( "\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape) )
+  lazy val hexDigit      = P( CharIn('0'to'9', 'a'to'f', 'A'to'F') )
+  lazy val unicodeEscape = P( "u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit )
+  lazy val escape        = P( "\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape) )
 
-  val strChars = P( CharsWhile(StringChars) )
-  val string =
+  lazy val strChars = P( CharsWhile(StringChars) )
+  lazy val string =
     P( space ~ "\"" ~/ (strChars | escape).rep.! ~ "\"").map(Js.Str)
 
-  val array =
+  lazy val array =
     P( "[" ~/ jsonExpr.rep(sep=",".~/) ~ space ~ "]").map(Js.Arr(_:_*))
 
-  val pair = P( string.map(_.value) ~/ ":" ~/ jsonExpr )
+  lazy val pair = P( string.map(_.value) ~/ ":" ~/ jsonExpr )
 
-  val obj =
+  lazy val obj =
     P( "{" ~/ pair.rep(sep=",".~/) ~ space ~ "}").map(Js.Obj(_:_*))
 
-  val jsonExpr: P[Js.Val] = P(
+  lazy val jsonExpr: P[Js.Val] = P(
     space ~ (obj | array | string | `true` | `false` | `null` | number) ~ space
   )
 }
