9e04b66d40ed211f0abe320d9e8ac7be5f957f7e
benjspriggs <benjspriggs@sprico.com>
2016-11-24 22:37:39 -0800

    update member view

--- src/main/resources/views/members.sql
+++ src/main/resources/views/members.sql
@@ -1,14 +1,18 @@
-create view MEMBER_VIEW
-AS
+CREATE VIEW `member_view` AS
   SELECT
-    MEMBERS.NUMBER,
-    MEMBERS.IS_SUSPENDED,
-    MEMBER_INFO.NAME,
-    LOCATIONS.CITY,
-    LOCATIONS.STATE,
-    LOCATIONS.STREET_ADDRESS,
-    LOCATIONS.ZIPCODE
-  FROM MEMBERS
-    JOIN MEMBER_INFO USING (NUMBER)
-    JOIN ENTITY_LOOKUP ON MEMBERS.NUMBER = ENTITY_LOOKUP.MEMBER_NUMBER
-    JOIN LOCATIONS ON ENTITY_LOOKUP.MEMBER_NUMBER = MEMBERS.NUMBER
+    `members`.`NUMBER`           AS `NUMBER`,
+    `members`.`IS_SUSPENDED`     AS `IS_SUSPENDED`,
+    `member_info`.`NAME`         AS `NAME`,
+    `locations`.`CITY`           AS `CITY`,
+    `locations`.`STATE`          AS `STATE`,
+    `locations`.`STREET_ADDRESS` AS `STREET_ADDRESS`,
+    `locations`.`ZIPCODE`        AS `ZIPCODE`
+  FROM `members`
+    JOIN `member_info`
+      ON `members`.`NUMBER` = `member_info`.`NUMBER`
+    JOIN `entity_lookup`
+      ON `members`.`NUMBER` = `entity_lookup`.`MEMBER_NUMBER`
+    JOIN `locations_lookup`
+      ON `locations_lookup`.`ENTITY_ID` = `entity_lookup`.`ID`
+    JOIN `locations`
+      ON `locations`.`ID` = `locations_lookup`.`LOCATION_ID`
