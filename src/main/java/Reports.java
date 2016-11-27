8457893045a919887c9b64100ed7e31358534437
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-27 12:56:00 -0800

    Moved code from WriteOutSummary to new method
    Moved code from WriteOutSummary to new method WriteSummaryReport

--- src/main/java/Reports.java
+++ src/main/java/Reports.java
@@ -136,4 +136,35 @@ public class Reports {
 
         return to_write;
     }
+
+    public String WriteSummaryReport (){
+        //Variables that will be written to the report. These will need to be updated to
+        //pull info from the sql database.
+
+        int total_prov = 0;
+        int total_consult = 0;
+        double week_fee = 0;
+
+        String prov_name = "test name";
+        int consult_num = 2;
+        double total_fee = 999.99;
+        String to_write = "";
+
+        total_prov += 1;
+        total_consult += consult_num;
+        week_fee += total_fee;
+
+        to_write += "Provider " + total_prov + "\n\t" + "Provider Name: " + prov_name +
+                "\n\t" + "Number of consultants for provider: " + Integer.toString(consult_num) +
+                "\n\t" + "Total fee for provider: " + Double.toString(total_fee) + "\n\n";
+
+        //Some sort of loop will be needed here to go over each provider.
+
+        //add totals
+        to_write += "Total amount of providers: " + Integer.toString(total_prov) + '\n' +
+                "Total amount of consultants: " + Integer.toString(total_consult) + '\n' +
+                "Total fee for the week: " + Double.toString(week_fee);
+
+        return to_write;
+    }
 }
