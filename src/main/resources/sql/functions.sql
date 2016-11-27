4834dec43f45a6cc41aca2ac4668ce385cb35b74
benjspriggs <benjspriggs@sprico.com>
2016-11-26 19:14:16 -0800

    add create_provider procedure

--- src/main/resources/sql/functions.sql
+++ src/main/resources/sql/functions.sql
@@ -16,3 +16,20 @@ BEGIN
   insert into locations_lookup(MEMBER_NUMBER, location_id) VALUES (`number`,
                                                                    LAST_INSERT_ID());
 END$$
+
+DELIMITER $$
+CREATE PROCEDURE `create_provider` (
+  IN number INT(11),
+  IN `name` VARCHAR(140),
+  IN `street_address` VARCHAR(50),
+  IN `city` VARCHAR(50),
+  IN `state` VARCHAR(2),
+  IN `zipcode` VARCHAR(32)
+)
+BEGIN
+    insert into providers (NUMBER, NAME) VALUES (`number`, `name`);
+    insert into locations (STREET_ADDRESS, CITY, STATE, ZIPCODE) VALUES
+      (`street_address`, `city`, `state`, `zipcode`);
+    insert into locations_lookup(PROVIDER_NUMBER, location_id) VALUES (`number`,
+                                                                     LAST_INSERT_ID());
+END$$
