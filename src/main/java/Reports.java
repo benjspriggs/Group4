2e416372f9ff4dbc05ec310965a861eb2905d655
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-27 12:50:33 -0800

    Moved code from WriteOutProviders to new method
    Moved code from WriteOutProviders to new method WriteProviderReport

--- src/main/java/Reports.java
+++ src/main/java/Reports.java
@@ -59,6 +59,7 @@ public class Reports {
                 serve_date + "\n\t" + "Provider Name: " + prov_name + "\n\t" + "Service Name: " +
                 service + "\n\n";
 
+        //second service for testing purposes. delete once sql access is created
         service_num = 2;
         serve_date = "01/03/16";
         prov_name = "test name2";
@@ -71,4 +72,68 @@ public class Reports {
         return to_write;
     }
 
+    public String WriteProviderReport (int id){
+        //Variables that will be written to the report. These will need to be updated to
+        //pull info from the sql database.
+
+        String name = "test name";
+        String address = "4321 street st";
+        String city = "Portland";
+        String state = "OR";
+        int zip = 54321;
+
+        //add provider info to report
+        String to_write = "Provider Name: " + name + '\n' + "Provider Number: " +
+                Integer.toString(id) + '\n' + "Provider Street Address: " + address +
+                '\n' + "Provider City: " + city + '\n' + "Provider State: " + state +
+                '\n' + "Provider Zip Code: " + Integer.toString(zip) + "\n\n";
+
+        double total_fee = 0;
+        int total_consult = 0;
+
+        //There will need to be some kind of loop here eventually to go through all services provided
+        int service_num = 1;
+        String serve_date = "01/02/16";
+        String comp_time = "01/02/16 12:13:22";
+        String mem_name = "Michael";
+        int mem_num = 123456789;
+        int serve_id = 123456;
+        double fee = 799.99;
+
+        total_fee += fee;
+        total_consult += 1;
+
+        //append provider info to report
+        to_write += "Service " + Integer.toString(service_num) + "\n\t" + "Service Date: " +
+                serve_date + "\n\t" + "Date and Time Computer Received Data: " + comp_time +
+                "\n\t" + "Member Name: " + mem_name + "\n\t" + "Member Number: " +
+                Integer.toString(mem_num) + "\n\t" + "Service Code: " + Integer.toString(serve_id) +
+                "\n\t" + "Fee: " + Double.toString(fee) + "\n\n";
+
+        //second service for testing purposes. delete once sql access is created
+        service_num = 2;
+        serve_date = "01/03/16";
+        comp_time = "01/02/16 11:12:21";
+        mem_name = "Michael";
+        mem_num = 123456789;
+        serve_id = 654321;
+        fee = 599.99;
+
+        total_fee += fee;
+        total_consult += 1;
+
+        to_write += "Service " + Integer.toString(service_num) + "\n\t" + "Service Date: " +
+                serve_date + "\n\t" + "Date and Time Computer Received Data: " + comp_time +
+                "\n\t" + "Member Name: " + mem_name + "\n\t" + "Member Number: " +
+                Integer.toString(mem_num) + "\n\t" + "Service Code: " + Integer.toString(serve_id) +
+                "\n\t" + "Fee: $" + Double.toString(fee) + "\n\n";
+
+
+        //append totals to report
+        to_write += "Total Number of Consultants With Members: " +
+                Integer.toString(total_consult) + '\n' + "Total Fee: $" +
+                Double.toString(total_fee);
+
+        return to_write;
+    }
 }
--- src/main/java/WriteToDisk.java
+++ src/main/java/WriteToDisk.java
@@ -17,81 +17,16 @@ public class WriteToDisk {
          }
     }
 
-    public void WriteOutProviders()
+    public void WriteOutProviders(String to_write, int id)
     {
-        //Variables that will be written to the report. These will need to be updated to
-        //pull info from the sql database.
-
-        String name = "test name";
-        int id = 987654321;
-        String address = "4321 street st";
-        String city = "Portland";
-        String state = "OR";
-        int zip = 54321;
-
-        //add provider info to report
-        String to_write = "Provider Name: " + name + '\n' + "Provider Number: " +
-                Integer.toString(id) + '\n' + "Provider Street Address: " + address +
-                '\n' + "Provider City: " + city + '\n' + "Provider State: " + state +
-                '\n' + "Provider Zip Code: " + Integer.toString(zip) + "\n\n";
-
-        double total_fee = 0;
-        int total_consult = 0;
-
-        //There will need to be some kind of loop here eventually to go through all services provided
-        int service_num = 1;
-        String serve_date = "01/02/16";
-        String comp_time = "01/02/16 12:13:22";
-        String mem_name = "Michael";
-        int mem_num = 123456789;
-        int serve_id = 123456;
-        double fee = 799.99;
-
-        total_fee += fee;
-        total_consult += 1;
-
-        //append provider info to report
-        to_write += "Service " + Integer.toString(service_num) + "\n\t" + "Service Date: " +
-                serve_date + "\n\t" + "Date and Time Computer Received Data: " + comp_time +
-                "\n\t" + "Member Name: " + mem_name + "\n\t" + "Member Number: " +
-                Integer.toString(mem_num) + "\n\t" + "Service Code: " + Integer.toString(serve_id) +
-                "\n\t" + "Fee: " + Double.toString(fee) + "\n\n";
-
-        service_num = 2;
-        serve_date = "01/03/16";
-        comp_time = "01/02/16 11:12:21";
-        mem_name = "Michael";
-        mem_num = 123456789;
-        serve_id = 654321;
-        fee = 599.99;
-
-        total_fee += fee;
-        total_consult += 1;
-
-        to_write += "Service " + Integer.toString(service_num) + "\n\t" + "Service Date: " +
-                serve_date + "\n\t" + "Date and Time Computer Received Data: " + comp_time +
-                "\n\t" + "Member Name: " + mem_name + "\n\t" + "Member Number: " +
-                Integer.toString(mem_num) + "\n\t" + "Service Code: " + Integer.toString(serve_id) +
-                "\n\t" + "Fee: $" + Double.toString(fee) + "\n\n";
-
-
-        //Add totals and append to report
-        to_write += "Total Number of Consultants With Members: " +
-                Integer.toString(total_consult) + '\n' + "Total Fee: $" +
-                Double.toString(total_fee);
-
         //write report to file
         WriteFile data = new WriteFile("ProviderReport" + Integer.toString(id) + ".txt");
         try {
             data.writeToFile(to_write);
-
         }
         catch (IOException e){
             System.out.println("IOException caught");
         }
-
-        //code will need to be written here to detect whether or not more
-        //more member reports will need to be written
     }
 
     public void WriteOutSummary()
