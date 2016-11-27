0c92dca5f3b15371027b80b4ce80af8b9d71e7e8
Michael <Michael>
2016-11-26 16:07:09 -0800

    Created WriteFile class

---
+++ src/main/java/WriteFile.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+/**
+ * Created by Michael Cohoe on 11/26/2016.
+ */
+
+public class WriteFile {
+    private String path;
+
+    public WriteFile( String file_path , boolean append_value ) {
+        path = file_path;
+    }
+}
