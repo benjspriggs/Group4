929b44d3056e594d28a98013708b1a19619930fc
benjspriggs <benjspriggs@sprico.com>
2016-11-26 19:12:37 -0800

    add create_member procedure

---
+++ src/main/resources/sql/functions.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+CREATE PROCEDURE `create_member` (
+  IN number INT(11),
+  IN is_suspended TINYINT(1),
+  IN `name` VARCHAR(140),
+  IN `street_address` VARCHAR(50),
+  IN `city` VARCHAR(50),
+  IN `state` VARCHAR(2),
+  IN `zipcode` VARCHAR(32)
+)
+BEGIN
+  insert into members (NUMBER, IS_SUSPENDED) VALUES (`number`, `is_suspended`);
+  insert into member_info (NUMBER, NAME) VALUES (`number`, `name`);
+  insert into locations (STREET_ADDRESS, CITY, STATE, ZIPCODE) VALUES
+    (`street_address`, `city`, `state`, `zipcode`);
+  insert into locations_lookup(MEMBER_NUMBER, location_id) VALUES (`number`,
+                                                                   LAST_INSERT_ID());
+END$$
