d947edbebe298d6922f6a830e3b43e4139a42062
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-27 12:42:19 -0800

    Moved code from WriteOutMember to new method WriteMemberReport

--- src/main/java/WriteToDisk.java
+++ src/main/java/WriteToDisk.java
@@ -4,44 +4,8 @@ import java.io.IOException;
  * Created by The Borg on 11/25/2016.
  */
 public class WriteToDisk {
-    public void WriteOutMember()
+    public void WriteOutMember(String to_write, int id)
     {
-        //Variables that will be written to the report. These will need to be updated to
-        //pull info from the sql database.
-
-        String name = "Michael";
-        int id = 123456789;
-        String address = "1234 street st";
-        String city = "Portland";
-        String state = "OR";
-        int zip = 12345;
-
-        //add member info to report
-        String to_write = "Member Name: " + name + '\n' + "Member Number: " + Integer.toString(id) +
-                '\n' + "Member Street Address: " + address + '\n' + "Member City: " + city + '\n' +
-                "Member State: " + state + '\n' + "Member Zip Code: " + Integer.toString(zip)
-                + "\n\n";
-
-        //There will need to be some kind of loop here eventually to go through all services provided
-        int service_num = 1;
-        String serve_date = "01/02/16";
-        String prov_name = "test name";
-        String service = "test service";
-
-        //append provider info to report
-        to_write += "Service " + Integer.toString(service_num) + "\n\t" + "Service Date: " +
-                serve_date + "\n\t" + "Provider Name: " + prov_name + "\n\t" + "Service Name: " +
-                service + "\n\n";
-
-        service_num = 2;
-        serve_date = "01/03/16";
-        prov_name = "test name2";
-        service = "test service2";
-
-        to_write += "Service " + Integer.toString(service_num) + "\n\t" + "Service Date: " +
-                serve_date + "\n\t" + "Provider Name: " + prov_name + "\n\t" + "Service Name: " +
-                service + '\n';
-
         //write report to file
         WriteFile data = new WriteFile("MemberReport" + Integer.toString(id) + ".txt");
          try {
@@ -51,10 +15,6 @@ public class WriteToDisk {
          catch (IOException e){
              System.out.println("IOException caught");
          }
-
-         //code will need to be written here to detect whether or not more
-         //more member reports will need to be written
-
     }
 
     public void WriteOutProviders()
