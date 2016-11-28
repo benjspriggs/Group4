eafaad21c7f9a0e8e7e9798d6a3c211d8b9e60b8
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-27 21:44:14 -0800

    Added more info to ServiceInfo

--- src/main/java/Reports/ServiceInfo.java
+++ src/main/java/Reports/ServiceInfo.java
@@ -4,18 +4,40 @@ package Reports;
  * Created by Michael Cohoe on 11/27/2016.
  */
 public class ServiceInfo {
-    private String serve_date = "01/02/16";
-    private String prov_name = "test name";
-    private String service = "test service";
+    private String serve_date;
+    private String timestamp;
+    private String prov_name;
+    private String service;
+    private int service_id;
+    private int mem_id;
+    private Double fee;
 
     public ServiceInfo(String serve_date, String prov_name, String service) {
         this.serve_date = serve_date;
+        this.timestamp = "";
         this.prov_name = prov_name;
         this.service = service;
+        this.service_id = 0;
+        this.mem_id = 0;
+        this.fee = 0.0;
+    }
+
+    public ServiceInfo(String serve_date, String timestamp, String prov_name, String service, int service_id, int mem_id, Double fee) {
+        this.serve_date = serve_date;
+        this.timestamp = timestamp;
+        this.prov_name = prov_name;
+        this.service = service;
+        this.service_id = service_id;
+        this.mem_id = mem_id;
+        this.fee = fee;
     }
 
     public String getServe_date() {
         return serve_date;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
     }
 
     public String getProv_name() {
@@ -24,5 +46,17 @@ public class ServiceInfo {
 
     public String getService() {
         return service;
+    }
+
+    public int getService_id() {
+        return service_id;
+    }
+
+    public int getMem_id() {
+        return mem_id;
+    }
+
+    public Double getFee() {
+        return fee;
     }
 }
