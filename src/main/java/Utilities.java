d4e8d5224aeef914f29449650d0c1c429a72876c
root <malice@pdx.edu>
2016-11-28 15:26:14 -0800

    Made the menus skeleton and Utilities class so we had a scanner

---
+++ src/main/java/Utilities.java
@@ -0,0 +1,16 @@
+/**
+ * Created by root on 11/28/16.
+ */
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Utilities {
+    protected static FileReader fileReader;
+    protected static Scanner input = null;
+
+    public Utilities() {
+        input = new Scanner(System.in);
+    }
+
+}
