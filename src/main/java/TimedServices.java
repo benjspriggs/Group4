d8543a3819e33e8706aeea09fbf0a47d474525c9
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-27 16:05:03 -0800

    Added method calls in timed services method.
    Currently does not write at timed intervals

--- src/main/java/TimedServices.java
+++ src/main/java/TimedServices.java
@@ -5,6 +5,9 @@ public class TimedServices {
 
     public void callWeeklySummary()
     {
-
+        Reports all_reports = new Reports();
+        all_reports.MemberSummaryReports(false);
+        all_reports.ProviderSummaryReports(false);
+        all_reports.SummerizeReports(false);
     }
 }
