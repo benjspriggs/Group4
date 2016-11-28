0186d3ee7840be74f1a973896866f33b5673b95d
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-27 19:34:32 -0800

    Added MemberInfo class that stores data

---
+++ src/main/java/MemberInfo.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Michael Cohoe on 11/27/2016.
+ */
+public class MemberInfo {
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+
+
+    public MemberInfo(String name, String address, String city, String state, int zip) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+}
---
+++ src/main/java/ProviderInfo.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Michael Cohoe on 11/27/2016.
+ */
+public class ProviderInfo {
+}
