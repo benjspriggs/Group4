bbbfdf5f5748eccfdb13376ac9a8b8c164f32333
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-27 19:35:05 -0800

    Added ServiceInfo class that stores data

--- src/main/java/ServiceInfo.java
+++ src/main/java/ServiceInfo.java
@@ -1,7 +1,26 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Michael Cohoe on 11/27/2016.
  */
 public class ServiceInfo {
+    private String serve_date = "01/02/16";
+    private String prov_name = "test name";
+    private String service = "test service";
+
+    public ServiceInfo(String serve_date, String prov_name, String service) {
+        this.serve_date = serve_date;
+        this.prov_name = prov_name;
+        this.service = service;
+    }
+
+    public String getServe_date() {
+        return serve_date;
+    }
+
+    public String getProv_name() {
+        return prov_name;
+    }
+
+    public String getService() {
+        return service;
+    }
 }
