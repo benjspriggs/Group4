c5bc25f23e65961938d90e27286c145185a0c849
Michael Cohoe <michaelccohoe@gmail.com>
2016-11-28 22:26:09 -0800

    Made timed services add current time to database

--- src/main/java/Reports/TimedServices.java
+++ src/main/java/Reports/TimedServices.java
@@ -2,6 +2,8 @@ package Reports;
 
 import sqldb.ChocanConnection;
 
+import java.sql.Timestamp;
+
 /**
  * Created by The Borg on 11/25/2016.
  */
@@ -16,6 +18,8 @@ public class TimedServices {
     public void callWeeklySummary()
     {
 
+        Timestamp time = new Timestamp(System.currentTimeMillis());
+        conn.addFileWriteDate(time);
         Reports all_reports = new Reports(conn);
         all_reports.MemberSummaryReports(false);
         all_reports.ProviderSummaryReports(false);
