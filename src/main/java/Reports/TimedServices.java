2fd45c8d4990025f9c483abb5298b21a182c05b9
Cameron Pick <yakri13@gmail.com>
2016-11-28 18:19:23 -0800

    Class outlines and function signatures version 1

--- src/main/java/Reports/TimedServices.java
+++ src/main/java/Reports/TimedServices.java
@@ -2,12 +2,25 @@ package Reports;
 
 import sqldb.ChocanConnection;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+import static java.util.Calendar.DAY_OF_WEEK;
+
 /**
  * Created by The Borg on 11/25/2016.
  */
 public class TimedServices {
 
     private ChocanConnection conn;
+    private Calendar with;
+    ScheduledExecutorService weeklyReportScheduler = Executors.newScheduledThreadPool(1);
+    //private timer weeklySummaryReport;
 
     public TimedServices(ChocanConnection conn){
         this.conn = conn;
@@ -20,5 +33,29 @@ public class TimedServices {
         all_reports.MemberSummaryReports(false);
         all_reports.ProviderSummaryReports(false);
         all_reports.SummarizeReports(false);
+    }
+
+    public void serviceTimer()
+    {
+        Map<Integer, Integer> dayOfWeekOffset = new HashMap<Integer, Integer>();
+        dayOfWeekOffset.put(Calendar.FRIDAY, 0);
+        dayOfWeekOffset.put(Calendar.SATURDAY, 6);
+        dayOfWeekOffset.put(Calendar.SUNDAY, 5);
+        dayOfWeekOffset.put(Calendar.MONDAY, 4);
+        dayOfWeekOffset.put(Calendar.TUESDAY, 3);
+        dayOfWeekOffset.put(Calendar.WEDNESDAY, 2);
+        dayOfWeekOffset.put(Calendar.THURSDAY, 1);
+        int dayOfWeek = with.get(DAY_OF_WEEK);
+        int delayInDays = dayOfWeekOffset.get(dayOfWeek);
+
+        weeklyReportScheduler.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                try {
+                    callWeeklySummary();
+                } catch (Exception ex) {
+                    ex.printStackTrace(); // or loggger would be better
+                }
+            }
+        }, delayInDays, 7, TimeUnit.DAYS);
     }
 }
