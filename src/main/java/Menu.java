76f5f4067bcf9946f7ad2d7f242b6b996007724c
root <malice@pdx.edu>
2016-11-29 16:42:46 -0800

    Made the menus skeleton and Utilities class so we had a scanner

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -12,6 +12,20 @@ public class Menu extends Utilities{
     public Menu(){    }
 
 
+    //Wrapper to call the correct menu type.  Takes an
+    // int to determine what kind of user.
+    //        1 == provider
+    //        2 == operator
+    //        3 == ChacAn account
+    public void displayMenu(int userType){
+        if(userType == 1)
+            providerMenu();
+        else if(userType == 2)
+            operatorMenu();
+        else if(userType == 3)
+            accountingMenu();
+    }
+
     public void providerMenu()
     {
         int userInput;
@@ -44,10 +58,11 @@ public class Menu extends Utilities{
         int userInput;
 
         System.out.println("Welcome ChocAn Operator!!! Please choose from the following options=D");
-        System.out.println("Create new membership           (1)");
-        System.out.println("Edit existing membership        (2)");
-        System.out.println("Delete existing membership      (3)");
-        System.out.println("View appointments for the week  (4)");
+        System.out.println("Create new membership                  (1)");
+        System.out.println("Edit existing membership               (2)");
+        System.out.println("Delete existing membership             (3)");
+        System.out.println("View appointments for the week         (4)");
+        System.out.println("View summary off all provider reports  (5)");
 
         userInput = input.nextInt();
 
@@ -58,6 +73,8 @@ public class Menu extends Utilities{
         else if(userInput == 3)
             ; //Wrapper goes here
         else if(userInput == 4)
+            ; //Wrapper goes here
+        else if(userInput == 5)
             ; //Wrapper goes here
         else
         {
