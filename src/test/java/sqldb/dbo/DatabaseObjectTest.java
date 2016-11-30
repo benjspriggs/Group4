8d899b98cf6e6f85da5e325070590ac0a6e904e8
benjspriggs <benjspriggs@sprico.com>
2016-11-30 12:26:36 -0800

    add tests for create

--- src/test/java/sqldb/dbo/DatabaseObjectTest.java
+++ src/test/java/sqldb/dbo/DatabaseObjectTest.java
@@ -1,12 +1,29 @@
 package sqldb.dbo;
 
+import org.easymock.Mock;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import static org.easymock.EasyMock.*;
 
 /**
  * Created by bspriggs on 11/30/2016.
  */
 abstract public class DatabaseObjectTest {
     abstract protected DatabaseObject getImplementation();
+    @Mock
+    PreparedStatement preparedStatement;
+
+    @Before
+    public void setUp() throws Exception {
+        preparedStatement = createMock(PreparedStatement.class);
+
+    }
+
     @Test
     public void create() throws Exception {
         assert getImplementation().create() != null;
@@ -29,12 +46,21 @@ abstract public class DatabaseObjectTest {
 
     @Test
     public void fillStatement() throws Exception {
-
+        for (DatabaseObject.DatabaseAction a : DatabaseObject.DatabaseAction.values()){
+            getImplementation().fillStatement(a, preparedStatement);
+        }
     }
 
     @Test
-    public void prepareStatement() throws Exception {
-
+    public void prepareCreateStatement() throws Exception {
+        DatabaseObject object = getImplementation();
+        Connection mockConn = createMock(Connection.class);
+        expect(mockConn.prepareStatement(object.create())).andStubReturn(preparedStatement);
+        replay(mockConn);
+        PreparedStatement expected = mockConn.prepareStatement(object.create());
+        Assert.assertSame(expected,
+                object
+                        .prepareStatement(DatabaseObject.DatabaseAction.CREATE, mockConn));
     }
 
 }
---
+++ src/test/java/sqldb/dbo/UserTest.java
@@ -0,0 +1,11 @@
+package sqldb.dbo;
+
+/**
+ * Created by bspriggs on 11/30/2016.
+ */
+public class UserTest extends DatabaseObjectTest {
+    @Override
+    protected DatabaseObject getImplementation() {
+        return new User(0, "username");
+    }
+}
