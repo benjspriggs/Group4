e28f86c8470c71e28d97ed50231d7b40c93bfca5
benjspriggs <benjspriggs@sprico.com>
2016-11-29 22:19:02 -0800

    add file for testing SqlAction

---
+++ src/test/java/interactive/action/SqlActionTest.java
@@ -0,0 +1,33 @@
+package interactive.action;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bspriggs on 11/29/2016.
+ */
+public class SqlActionTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void execute() throws Exception {
+
+    }
+
+    @Test
+    public void executeAndReturn() throws Exception {
+
+    }
+
+}
