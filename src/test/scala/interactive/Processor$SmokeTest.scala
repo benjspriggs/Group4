078a5f2077ca893ce44bc52e36fa0fed83e0c248
benjspriggs <benjspriggs@sprico.com>
2016-11-30 16:48:38 -0800

    sketch unit tests for processor

---
+++ src/test/scala/interactive/Processor$SmokeTest.scala
@@ -0,0 +1,30 @@
+package interactive
+
+import org.scalatest.FlatSpec
+
+/**
+  * Created by bspriggs on 11/30/2016.
+  */
+class Processor$SmokeTest extends FlatSpec {
+
+  behavior of "processor"
+
+  ignore must "handle requests to create a user" in
+  {
+
+  }
+
+  ignore must "handle requests to create a member" in
+  {
+
+  }
+
+  ignore must "handle requests to delete a service" in
+  {
+
+  }
+
+  ignore must "handle requests to stop" in {
+
+  }
+}
