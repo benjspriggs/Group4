16051ef13efd9c8f6100b2001ecd2e7e456e7437
benjspriggs <benjspriggs@sprico.com>
2016-11-30 16:57:54 -0800

    add statement object

---
+++ src/main/scala/interactive/Statements.scala
@@ -0,0 +1,41 @@
+package interactive
+
+import interactive.Tokens.{Obj, Request, SuperObj}
+
+/**
+  * Created by bspriggs on 11/30/2016.
+  */
+object Statements {
+  /**
+    * Allows a user to gain more information.
+    * about a reserved word or the syntax of the Interactive Mode.
+    * @param value Whatever the user wants to know, from the reserved words
+    *              or keywords
+    */
+  case class Help(value: Option[String]) extends AnyVal
+
+  /**
+    * A request that applies to potentially
+    * multiple objects.
+    * @param value A tuple with the request and modified general object
+    */
+  case class Poly(value: (Request, SuperObj)) extends AnyVal
+
+  /**
+    * A request that applies to discrete objects,
+    * that are each unique.
+    * @param value A sequence of tuples with the request and object(s)
+    */
+  case class Mono(value: (Request, Obj)*) extends AnyVal
+
+  /**
+    * Literal SQL, when the grammar just isn't cutting it.
+    * @param value The literal SQL query, baretext or quoted
+    */
+  case class SQL(value: String) extends AnyVal
+
+  /**
+    * A request to stop a session.
+    */
+  case object Stop
+}
