dba4634f38a82e39f68cc9f72e499bd1ab6601a7
benjspriggs <benjspriggs@sprico.com>
2016-11-30 22:35:49 -0800

    add blank method, and smoke tests for parser

---
+++ src/main/scala/interactive/Processor.scala
@@ -0,0 +1,8 @@
+package interactive
+
+/**
+  * Created by bspriggs on 11/30/2016.
+  */
+class Processor {
+  def process(d: Any) = Unit
+}
--- src/test/scala/interactive/Processor$SmokeTest.scala
+++ src/test/scala/interactive/Processor$SmokeTest.scala
@@ -1,41 +1,81 @@
 package interactive
 
+import java.sql.Connection
+
 import interactive.Statements._
 import interactive.Term._
+import interactive.action.SqlAction
+import org.scalatest.mockito.MockitoSugar
+import sqldb.dbo.DatabaseObject.DatabaseAction
+import sqldb.dbo.{Location, Member, Provider, User}
 
 /**
   * Created by bspriggs on 11/30/2016.
   */
-class Processor$SmokeTest extends InteractiveModeFixtures {
+class Processor$SmokeTest extends InteractiveModeFixtures with MockitoSugar{
 
   behavior of "processor"
+  val processor = new Processor()
+  val connection: Connection = mock[Connection]
 
-  ignore must "handle requests to create a user" in
-  {
-      val createUser = Mono((
-        Request("create"),
-          Obj((Type.One("user"), Seq(f.parsedJson())))
-        )
-      )
+  ignore must "handle requests to create a user" in {
+    val createUser = Mono((
+      Request("create"),
+      Obj((Type.One("user"), Seq(f.parsedJson("{ \"username\": \"test\" }")))))
+    )
+    val expectedSqlAction = new SqlAction[User](
+      connection, new User(0, "test"), DatabaseAction.CREATE)
+    assertResult(processor.process(createUser))(Some(expectedSqlAction))
   }
 
-  ignore must "handle requests to create a member" in
-  {
+  ignore must "handle requests to create a member" in {
     val createMember = Mono((
       Request("create"),
-      Obj((Type.One("member"), Seq(f.parsedJson()))) )
-    )
+      Obj((Type.One("member"), Seq(
+        f.parsedJson(
+          """
+            | {
+            | "name": "name",
+            | "is_suspended": false,
+            | "street_address": "street_address",
+            | "city": "city",
+            | "state": "state",
+            | "zipcode": "zipcode"
+            | }
+          """.stripMargin)
+      )) )
+    ))
+    val location = new Location(0, "street_address", "city", "state", "zipcode")
+    val expectedSqlAction = new SqlAction[Member](
+      connection, new Member(0, "name", false, location), DatabaseAction.CREATE)
+    assertResult(processor.process(createMember))(Some(expectedSqlAction))
   }
 
-  ignore must "handle requests to delete a service" in
-  {
-    val deleteService = Mono((
+  ignore must "handle requests to delete a provider" in {
+    val deleteProvider = Mono((
       Request("delete"),
-      Obj((Type.One("service"), Seq(f.parsedJson()))))
+      Obj((Type.One("provider"), Seq(
+        f.parsedJson(
+          """
+            | {
+            | "name": "name",
+            | "is_suspended": false,
+            | "street_address": "street_address",
+            | "city": "city",
+            | "state": "state",
+            | "zipcode": "zipcode"
+            | }
+          """.stripMargin)
+      ))))
     )
+
+    val location = new Location(0, "street_address", "city", "state", "zipcode")
+    val expectedSqlAction = new SqlAction[Provider](
+      connection, new Provider(0, "name", location), DatabaseAction.CREATE)
+    assertResult(processor.process(deleteProvider))(Some(expectedSqlAction))
   }
 
   ignore must "handle requests to stop" in {
-
+    assertResult(processor.process(Stop))(None)
   }
 }
