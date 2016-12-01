1a6cbab277e1b5ab1f3a34a55a676d3a13cb5f63
benjspriggs <benjspriggs@sprico.com>
2016-11-30 16:59:31 -0800

    remove term mixin

--- src/main/scala/interactive/Tokens.scala
+++ src/main/scala/interactive/Tokens.scala
@@ -8,65 +8,40 @@ import interactive.parser.JsonParser
   * Interactive Mode AST Tokens
   */
 object Tokens {
-  sealed trait Term extends Any {
-    def value: Any
-  }
-
-  /**
-    * Allows a user to gain more information.
-    * about a reserved word or the syntax of the Interactive Mode.
-    * @param value Whatever the user wants to know, from the reserved words
-    *              or keywords
-    */
-  case class Help(value: Option[java.lang.String]) extends AnyVal with Term
-
   /**
     * A JSON object with a clarifying type.
     * e.g. user { "name": "Barack Obama" }
     * @param value A list of type and Payload tuple pairs
     */
-  case class Obj(value: (Type.One, Payload)*) extends AnyVal with Term
+  case class Obj(value: (Type.One, Payload)*) extends AnyVal
 
   /**
     * A JSON object with a pluralizing type.
     * e.g. users { "status": "valid" }
     * @param value A plural type and Payload pair
     */
-  case class SuperObj(value: (Type.Many, Option[Payload])) extends AnyVal with Term
+  case class SuperObj(value: (Type.Many, Option[Payload])) extends AnyVal
 
   /**
     * CRUD operations, and write-to-disk operations.
     * @param value The requested operation
     */
-  case class Request(value: java.lang.String) extends AnyVal with Term
-
-  /**
-    * Literal SQL, when the grammar just isn't cutting it.
-    * @param value The literal SQL query, baretext or quoted
-    */
-  case class SQL(value: java.lang.String) extends AnyVal with Term
+  case class Request(value: java.lang.String) extends AnyVal
 
   type Payload = JsonParser.Js.Val
 
   object Type {
-
     /**
       * A single user, member, etc.
       * @param value Object type
       */
-    case class One(value: java.lang.String) extends AnyVal with Term
+    case class One(value: java.lang.String) extends AnyVal
 
     /**
       * A plural user, member, etc.
       * @param value Object type
       */
-    case class Many(value: java.lang.String) extends AnyVal with Term
+    case class Many(value: java.lang.String) extends AnyVal
   }
 
-  /**
-    * A request to stop a session.
-    */
-  case object Stop extends Term {
-    def value = Stop
-  }
 }
