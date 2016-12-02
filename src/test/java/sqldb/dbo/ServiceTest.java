6891c986403c39f523ea5e1f1ef0693602d167d1
benjspriggs <benjspriggs@sprico.com>
2016-12-01 17:10:31 -0800

    fill service constructor

--- src/test/java/sqldb/dbo/ServiceTest.java
+++ src/test/java/sqldb/dbo/ServiceTest.java
@@ -1,12 +1,15 @@
 package sqldb.dbo;
 
+import java.math.BigDecimal;
+
 /**
  * Created by bspriggs on 12/1/2016.
  */
 public class ServiceTest extends DatabaseObjectTest {
     @Override
     protected DatabaseObject getImplementation() {
-        return new Service();
+        return new Service(0, "service_name",
+                new BigDecimal(0.00), "description");
     }
 
     @Override
