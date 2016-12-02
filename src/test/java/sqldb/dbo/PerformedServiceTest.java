88b11f6b7c403610a920b32c862bf0228017388f
benjspriggs <benjspriggs@sprico.com>
2016-12-01 18:09:47 -0800

    use proper date in test, Fixes #4

--- src/test/java/sqldb/dbo/PerformedServiceTest.java
+++ src/test/java/sqldb/dbo/PerformedServiceTest.java
@@ -1,7 +1,7 @@
 package sqldb.dbo;
 
 import java.sql.Timestamp;
-import java.util.Date;
+import java.sql.Date;
 
 /**
  * Created by bspriggs on 12/1/2016.
@@ -10,7 +10,7 @@ public class PerformedServiceTest extends DatabaseObjectTest {
     @Override
     protected DatabaseObject getImplementation() {
         return new PerformedService(0,
-                new Date(),
+                new Date(System.currentTimeMillis()),
                 new Timestamp(System.currentTimeMillis()),
                 "comments");
     }
