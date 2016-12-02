ae5ee5ce6d67fc23f91b8e9676c1a59e24a45863
benjspriggs <benjspriggs@sprico.com>
2016-12-01 17:39:05 -0800

    update constructor and usages for performed service to reference member and provider information

--- src/test/java/sqldb/dbo/PerformedServiceTest.java
+++ src/test/java/sqldb/dbo/PerformedServiceTest.java
@@ -10,6 +10,9 @@ public class PerformedServiceTest extends DatabaseObjectTest {
     @Override
     protected DatabaseObject getImplementation() {
         return new PerformedService(0,
+                0,
+                0,
+                0,
                 new Date(),
                 new Timestamp(System.currentTimeMillis()),
                 "comments");
