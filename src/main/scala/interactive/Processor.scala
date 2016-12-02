1e8692936720b94689a35e1251f537b59c42b5e0
benjspriggs <benjspriggs@sprico.com>
2016-12-01 20:19:10 -0800

    add private handlers, optimize imports probably

--- src/main/scala/interactive/Processor.scala
+++ src/main/scala/interactive/Processor.scala
@@ -1,7 +1,7 @@
 package interactive
 
-import interactive.action.{Action, ReturnableAction, SqlAction}
-import sqldb.dbo.User
+import interactive.Term.{Obj, Request, SuperObj}
+import interactive.action._
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -12,21 +12,33 @@ class Processor {
   // Process a sequence of statements to SQL or File Actions
   def process(statements: ArrayBuffer[_]) = Unit
 
+  private def handleMonoRequest[R](request: Request, objs: Obj):
+    Option[ReturnableAction[R]] = {
+    None
+  }
+
+  private def handlePolyRequest[R](request: Request, superobj: SuperObj):
+    Option[ReturnableAction[R]] = {
+    None
+  }
+
   // Process a  statements to a SQL or File Action
   def process[R](statement: Statement): Option[ReturnableAction[R]] =
-  statement match {
-    case Stop => None
-    case Help(string) => None // TODO: Add 'display help' action
-    case SQL(query) => None // TODO: Add generic execute SQL action
-    case Mono((request, objs)) => {
-      None
+    statement match {
+      case Stop => None
+      case Help(string) => None // TODO: Add 'display help' action
+      case SQL(query) => None // TODO: Add generic execute SQL action
+      case Mono((request, objs)) => {
+        handleMonoRequest[R](request, objs)
+      }
+      case Poly((request, superobj)) => {
+        handlePolyRequest[R](request, superobj)
+      }
     }
-    case Poly((request, superobj)) => {
-      None
-    }
-  }
 
   def loadClass(classname: String): Class[_] = {
     Class.forName("sqldb.dbo." + classname)
   }
+
+
 }
